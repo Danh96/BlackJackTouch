dee363a6ffe44aebe0805fb2f5fbc81372c2709a
Daniel Hayes <daniel.hayes@aviva.com>
2018-05-22 10:39:45 +0100

    Deck functionality added

---
+++ BlackJackIOS/Card.cs
@@ -0,0 +1,15 @@
+﻿namespace DeckOfCards
+{
+    public class Card
+    {
+        public int Value { get; }
+
+        public CardSuit Suit { get; }
+
+        public Card(int value, CardSuit suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+    }
+}
